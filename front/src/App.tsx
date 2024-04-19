656fab4234fac08d6f4c6b3c82d702dbb484535f
Tanguy Gibrat <twnguy@MacBook-Pro-de-Tanguy.local>
2024-04-19 16:31:32 +0200

    front: config TailwindCSS + login page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,25 +1,10 @@
-import { useState } from 'react'
-import { Navbar } from './Navbar'
+import { Login } from './Login'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Navbar counter={count} />
-      <h1>Vite + React</h1>
-      <div className="card" style={{ height: "300px", backgroundColor: "red" }}>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Login />
     </>
   )
 }
