71de98d447eb572baf6a160de4267927839d195c
Romain Farinacci <saskoue@MacBook-Pro-de-Romain.local>
2024-04-19 18:25:13 +0200

    Calendar close modal fix

--- front/src/components/Calendar.tsx
+++ front/src/components/Calendar.tsx
@@ -160,7 +160,9 @@ export const Calendar: React.FC = () => {
                         <div className="p-4 max-w-xl mx-auto relative absolute left-0 right-0 overflow-hidden mt-24">
                             <div className="shadow absolute right-0 top-0 w-10 h-10 rounded-full bg-white text-gray-500 hover:text-gray-800 inline-flex items-center justify-center cursor-pointer"
                                 onClick={() => setOpenEventModal(false)}>
-                                {/* Close Icon */}
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                </svg>
                             </div>
 
                             <div className="shadow w-full rounded-lg bg-white overflow-hidden w-full block p-8">
