49792abd84298d4c14bd079f9fefd2c74389a97f
Tanguy Gibrat <twnguy@MacBook-Pro-de-Tanguy.local>
2024-04-19 17:36:31 +0200

    front: add components & assets folders

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Navbar } from './Navbar';
-import { Footer } from './Footer';
-import './App.css';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import './styles/App.css';
 
 const App: React.FC = () => {
   return (
     <>
-    <Navbar />
-    <Footer />
+      <Navbar />
+      <Footer />
     </>
   );
 };
---
+++ front/src/components/Navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Login } from './Login';
+
+export const Navbar: React.FC = () => {
+  return (
+    <Router>
+      <div>
+        <nav className="bg-gray-800 p-2 min-h-max">
+          <div className="max-w-7xl mx-auto px-4">
+            <div className="flex items-center justify-between h-16">
+              <div className="flex items-center">
+                <Link to="/" className="text-white text-xl mr-12 hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium">Hackaton App</Link>
+              </div>
+              <div>
+                <Link to="/auth" className="text-white hover:text-gray-300 px-3 py-2 rounded-md text-sm font-medium">Accès membre</Link>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/auth" Component={Login as React.ComponentType} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
