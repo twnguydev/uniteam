febdde8db5b0311919ad86b221a4687a7c277832
Romain Farinacci <saskoue@MacBook-Pro-de-Romain.local>
2024-04-19 19:22:48 +0200

    fixed modal calendar date

--- front/src/components/Calendar.tsx
+++ front/src/components/Calendar.tsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from 'react';
+import { format } from 'date-fns';
+import { fr } from 'date-fns/locale';
 
 const MONTH_NAMES = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'];
 const DAYS = ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'];
@@ -10,11 +12,11 @@ interface Event {
 }
 
 const themes = [
-    { value: "blue", label: "Blue Theme" },
-    { value: "red", label: "Red Theme" },
-    { value: "yellow", label: "Yellow Theme" },
-    { value: "green", label: "Green Theme" },
-    { value: "purple", label: "Purple Theme" },
+    { value: "blue", label: "Bleu" },
+    { value: "red", label: "Rouge" },
+    { value: "yellow", label: "Jaune" },
+    { value: "green", label: "Vert" },
+    { value: "purple", label: "Violet" },
 ];
 
 export const Calendar: React.FC = () => {
@@ -42,13 +44,15 @@ export const Calendar: React.FC = () => {
     const [openEventModal, setOpenEventModal] = useState(false);
     const [eventTitle, setEventTitle] = useState('');
     const [eventDate, setEventDate] = useState<Date | null>(null);
+    const formatDate = (date: Date) => {
+        return format(date, "EEEE, d MMMM yyyy", { locale: fr });
+    };
     const [eventTheme, setEventTheme] = useState('blue');
 
     useEffect(() => {
         function getNoOfDays() {
             let daysInMonth = new Date(year, month + 1, 0).getDate();
 
-            // Start day of the month
             let dayOfWeek = new Date(year, month).getDay();
             let blankdaysArray = [];
             for (var i = 0; i < dayOfWeek; i++) {
@@ -129,12 +133,12 @@ export const Calendar: React.FC = () => {
                             {noOfDays.map((date, index) => (
                                 <div key={index} style={{ width: '14.28%', height: '120px' }} className="px-4 pt-2 border-r border-b relative">
                                     <div
-                                        onClick={() => { setOpenEventModal(true); setEventDate(new Date(year, month, date)); }}
+                                        onClick={() => { setOpenEventModal(true); setEventDate(new Date(date, month, year)); }}
                                         className={`inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100 ${new Date(year, month, date).toDateString() === new Date().toDateString() ? 'bg-blue-500 text-white' : 'text-gray-700 hover:bg-blue-200'}`}>
                                         {date}
                                     </div>
                                     <div style={{ height: '80px' }} className="overflow-y-auto mt-1">
-                                        {events.filter(e => new Date(e.event_date).toDateString() === new Date(year, month, date).toDateString()).map((event, idx) => (
+                                        {events.filter(e => new Date(e.event_date).toDateString() === new Date(date, month, year).toDateString()).map((event, idx) => (
                                             <div
                                                 key={idx}
                                                 className={`px-2 py-1 rounded-lg mt-1 overflow-hidden border ${{
@@ -154,7 +158,6 @@ export const Calendar: React.FC = () => {
                     </div>
                 </div>
 
-                {/* Modal */}
                 {openEventModal && (
                     <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.8)' }} className="fixed z-40 top-0 right-0 left-0 bottom-0 h-full w-full">
                         <div className="p-4 max-w-xl mx-auto relative absolute left-0 right-0 overflow-hidden mt-24">
@@ -166,20 +169,20 @@ export const Calendar: React.FC = () => {
                             </div>
 
                             <div className="shadow w-full rounded-lg bg-white overflow-hidden w-full block p-8">
-                                <h2 className="font-bold text-2xl mb-6 text-gray-800 border-b pb-2">Ajouter un événement</h2>
+                                <h2 className="font-bold text-2xl mb-6 text-gray-800 border-b pb-2">Nouvel Evenement</h2>
 
                                 <div className="mb-4">
-                                    <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Nom de l'événement</label>
+                                    <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Titre de l'evenement</label>
                                     <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value={eventTitle} onChange={(e) => setEventTitle(e.target.value)} />
                                 </div>
 
                                 <div className="mb-4">
-                                    <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Date</label>
-                                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value={eventDate?.toDateString() || ''} readOnly />
+                                    <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Date de l'evenement</label>
+                                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value={eventDate ? formatDate(eventDate) : ''} readOnly />
                                 </div>
 
                                 <div className="inline-block w-64 mb-4">
-                                    <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Couleur du badge</label>
+                                    <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Couleur</label>
                                     <div className="relative">
                                         <select className="block appearance-none w-full bg-gray-200 border-2 border-gray-200 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-blue-500 text-gray-700"
                                             value={eventTheme}
@@ -199,14 +202,13 @@ export const Calendar: React.FC = () => {
                                         Annuler
                                     </button>
                                     <button type="button" className="bg-gray-800 hover:bg-gray-700 text-white font-semibold py-2 px-4 border border-gray-700 rounded-lg shadow-sm" onClick={addEvent}>
-                                        Ajouter
+                                        Enregistrer
                                     </button>
                                 </div>
                             </div>
                         </div>
                     </div>
                 )}
-                {/* /Modal */}
             </div>
         </div>
     );
