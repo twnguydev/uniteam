e70730b15208d884ce68d2c67683e9c79739437b
Tanguy Gibrat <twnguy@MacBook-Pro-de-Tanguy.local>
2024-04-20 10:08:56 +0200

    front: fix calendar

--- front/src/components/Calendar.tsx
+++ front/src/components/Calendar.tsx
@@ -157,7 +157,7 @@ export const Calendar: React.FC = () => {
                         </div>
 
                         <div className="shadow w-full rounded-lg bg-white overflow-hidden block max-h-[80vh] overflow-y-auto p-8">
-                            <h2 className="font-bold text-2xl mb-6 text-gray-800 border-b pb-2">Nouvel Événement</h2>
+                            <h2 className="font-bold text-2xl mb-6 text-gray-800 border-b pb-2">Ajouter un événement</h2>
 
                             <div className="mb-4">
                                 <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Titre de l'événement</label>
@@ -167,7 +167,7 @@ export const Calendar: React.FC = () => {
 
                             <div className="mb-4">
                                 <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Description de l'événement</label>
-                                <textarea name="eventDesc" rows="4" className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 resize-none"
+                                <textarea name="eventDesc" rows={4} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 resize-none"
                                     value={eventDesc} onChange={(e) => setEventDesc(e.target.value)}/>
                             </div>
                             
@@ -198,7 +198,7 @@ export const Calendar: React.FC = () => {
                                     type="time" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
                             </div>
                             <div className="inline-block w-64 mb-4">
-                                <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Thème de l'événement</label>
+                                <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Groupe</label>
                                 <select className="block appearance-none w-full bg-gray-200 border-2 border-gray-200 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-blue-500 text-gray-700"
                                     value={eventTheme} onChange={(e) => setEventTheme(e.target.value)}>
                                     {themes.map((theme, index) => (
