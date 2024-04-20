0813293b1560040a13b84ca123b4c3400121c639
Tanguy Gibrat <twnguy@MacBook-Pro-de-Tanguy.local>
2024-04-20 16:08:14 +0200

    front: fix calendar

--- front/src/components/Calendar.tsx
+++ front/src/components/Calendar.tsx
@@ -95,7 +95,7 @@ export const Calendar: React.FC = () => {
 
     return (
         <div className="antialiased sans-serif bg-gray-100 h-screen">
-            <div className="container mx-auto px-4 py-2 md:py-24">
+            <div className="container-fluid mx-auto px-4 py-2 md:py-24">
                 <div className="bg-white rounded-lg shadow overflow-hidden">
                     <div className="flex items-center justify-between py-2 px-6">
                         <div>
@@ -157,11 +157,11 @@ export const Calendar: React.FC = () => {
                             <h2 className="font-bold text-2xl mb-6 text-gray-800 border-b pb-2">Ajouter un événement</h2>
                             <div className="mb-4">
                                 <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Titre de l'événement</label>
-                                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" value={eventTitle} onChange={(e) => setEventTitle(e.target.value)} />
+                                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="text" onChange={(e) => setEventTitle(e.target.value)} />
                             </div>
                             <div className="mb-4">
                                 <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Description de l'événement</label>
-                                <textarea name="eventDesc" rows={4} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 resize-none" value={eventDesc} onChange={(e) => setEventDesc(e.target.value)}/>
+                                <textarea name="eventDesc" rows={4} className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500 resize-none" onChange={(e) => setEventDesc(e.target.value)}/>
                             </div>
                             <div className="flex justify-between space-x-4 mb-4">
                                 <div className="w-2/3">
@@ -170,17 +170,17 @@ export const Calendar: React.FC = () => {
                                 </div>
                                 <div className="w-1/3">
                                     <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Heure de début</label>
-                                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="time" value={startTime} onChange={(e) => setStartTime(e.target.value)} />
+                                    <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="time" onChange={(e) => setStartTime(e.target.value)} />
                                 </div>
                             </div>
                             <div className="mb-4">
                                 <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Heure de fin</label>
-                                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="time" value={endTime} onChange={(e) => setEndTime(e.target.value)} />
+                                <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded-lg w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" type="time" onChange={(e) => setEndTime(e.target.value)} />
                             </div>
                             <div className="flex justify-between space-x-4 mb-4">
                                 <div className='w-2/3'>
                                     <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Groupe</label>
-                                    <select className="block appearance-none w-full bg-gray-200 border-2 border-gray-200 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-blue-500 text-gray-700" value={eventTheme} onChange={(e) => setEventTheme(e.target.value)}>
+                                    <select className="block appearance-none w-full bg-gray-200 border-2 border-gray-200 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-blue-500 text-gray-700" onChange={(e) => setEventTheme(e.target.value)}>
                                         {themes.map((theme, index) => (
                                             <option key={index} value={theme.value}>{theme.label}</option>
                                         ))}
@@ -188,7 +188,7 @@ export const Calendar: React.FC = () => {
                                 </div>
                                 <div className='w-1/3'>
                                     <label className="text-gray-800 block mb-1 font-bold text-sm tracking-wide">Salle</label>
-                                    <select className="block appearance-none w-full bg-gray-200 border-2 border-gray-200 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-blue-500 text-gray-700" value={eventRoom} onChange={(e) => setEventRoom(e.target.value)}>
+                                    <select className="block appearance-none w-full bg-gray-200 border-2 border-gray-200 hover:border-gray-500 px-4 py-2 pr-8 rounded-lg leading-tight focus:outline-none focus:bg-white focus:border-blue-500 text-gray-700" onChange={(e) => setEventRoom(e.target.value)}>
                                         {rooms.map((room, index) => (
                                             <option key={index} value={room.value}>{room.label}</option>
                                         ))}
