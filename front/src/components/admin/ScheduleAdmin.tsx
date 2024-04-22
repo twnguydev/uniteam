05be326410ee8ced6f433499c35e2624fb1ed805
Romain Farinacci <saskoue@MacBook-Pro-de-Romain.local>
2024-04-22 09:56:29 +0200

    front: calendar select fix based on modal

--- front/src/components/admin/ScheduleAdmin.tsx
+++ front/src/components/admin/ScheduleAdmin.tsx
@@ -5,7 +5,6 @@ import { ListUsersAdmin } from './ListUsersAdmin';
 import { ListEventsAdmin } from './ListEventsAdmin';
 
 import { findAllEvents } from '../../utils/event';
-
 import { findAllGroups, findGroupId } from '../../utils/group';
 
 import type { Group } from '../../types/group';
@@ -16,10 +15,8 @@ export const ScheduleAdmin: React.FC = () => {
 
     const [isUserModalOpen, setIsUserModalOpen] = useState(false);
     const [isEventModalOpen, setIsEventModalOpen] = useState(false);
-    const [events, setEvents ] = useState<Event[]>([]);
-
+    const [events, setEvents] = useState<Event[]>([]);
     const [selectedGroup, setSelectedGroup] = useState<string>('');
-
     const [loadedGroups, setLoadedGroups] = useState<Group[]>([]);
 
     const handleGroupChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -36,19 +33,18 @@ export const ScheduleAdmin: React.FC = () => {
             const selectedGroupId: number | undefined = await findGroupId(selectedGroup, user);
             const allEvents: any = await findAllEvents<User>(user);
             let filteredEvents: any = allEvents;
-    
+
             if (selectedGroup) {
                 filteredEvents = allEvents.filter((event: any) => event.groupId === selectedGroupId);
             }
             setEvents(filteredEvents || []);
-    
+
             const fetchedGroups: any = await findAllGroups<User>(user);
             setLoadedGroups(fetchedGroups || []);
         };
-    
+
         fetchData();
     }, [user, selectedGroup]);
-
 
     const toggleUserModal = (): void => {
         setIsUserModalOpen(true);
@@ -85,7 +81,6 @@ export const ScheduleAdmin: React.FC = () => {
                     <h2 className="text-4xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">
                         Espace administrateur
                     </h2>
-
                     <div className="mt-4">
                         <div className="inline-flex items-center text-lg font-medium text-primary-600 text-blue-500">
                             Il vous permet de gérer les événements et les utilisateurs.
@@ -97,20 +92,22 @@ export const ScheduleAdmin: React.FC = () => {
                     <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600 mt-4 ml-4" onClick={toggleUserModal}>
                         Afficher les utilisateurs
                     </button>
-                    <form className="max-w-sm mx-auto mt-6">
-                        <label htmlFor="underline_select" className="sr-only">Groupe</label>
-                        <select
-                            id="underline_select"
-                            className="block appearance-none w-full bg-gray-700 border-2 border-gray-900 hover:border-gray-500 px-2 py-1.5 pr-4 rounded-lg leading-tight text-gray-200"
-                            value={selectedGroup}
-                            onChange={handleGroupChange}
-                        >
-                        <option value="">Sans filtre</option>
-                            {loadedGroups.map(group => (
-                                <option key={group.id} value={group.name}>{group.name}</option>
-                            ))}
+                    {isEventModalOpen && (
+                        <form className="max-w-sm mx-auto mt-6">
+                            <label htmlFor="underline_select" className="sr-only">Groupe</label>
+                            <select
+                                id="underline_select"
+                                className="block appearance-none w-full bg-gray-700 border-2 border-gray-900 hover:border-gray-500 px-2 py-1.5 pr-4 rounded-lg leading-tight text-gray-200"
+                                value={selectedGroup}
+                                onChange={handleGroupChange}
+                            >
+                            <option value="">Sans filtre</option>
+                                {loadedGroups.map(group => (
+                                    <option key={group.id} value={group.name}>{group.name}</option>
+                                ))}
                             </select>
                         </form>
+                    )}
                     {isUserModalOpen && <ListUsersAdmin />}
                     {isEventModalOpen && <ListEventsAdmin selectedGroup={selectedGroup} />}
                 </div>
